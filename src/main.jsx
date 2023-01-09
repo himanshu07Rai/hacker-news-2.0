05a1b6b493577f0c5e62f1c77ec8dffffa1472a4
Himanshu Rai <raihimanshu22345gmail.com>
2023-01-09 16:34:18 +0530

    infinite scroll working properly, need to work on date change

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>
 );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,80 +1,85 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+let day = 7;
+
+const fetchData = async (setData, data, setError) => {
+  try {
+    const res = await axios.get(`/api/data/2023010${day}.js`, {
+      method: "GET",
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+      credentials: "same-origin",
+    });
+    const t = res.data;
+    //   let x = t;
+    //   x.sort((a, b) => b.points - a.points);
+    setData([...data, ...t]);
+    day = day - 1;
+  } catch (error) {
+    setError(error);
+  }
+};
 
 const Homepage = () => {
   const [data, setData] = useState([]);
-  const [query, setQuery] = useState("");
-  const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [commentsCount, setCommentsCount] = useState(0);
 
-  const baseURL = "https://hckrnews.com/";
+  const date = new Date(2023, 0, 1);
+  console.log(date.getDate(), date.getMonth(), date.getFullYear());
+
+  date.setDate(date.getDate() - 1);
+
+  console.log(date.getDate(), date.getMonth(), date.getFullYear());
+
+  console.log(date);
 
   useEffect(() => {
-    fetchData();
-  }, [search]);
+    setIsLoading(true);
+    fetchData(setData, data, setError);
+    setIsLoading(false);
+  }, []);
 
   console.log(error);
 
-  const fetchData = async () => {
-    try {
-      const res = await axios.get(`/api/data/20230107.js`, {
-        method: "GET",
-        mode: "no-cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-        credentials: "same-origin",
-      });
-      const t = res.data;
-      setData(t);
-
-      fetchData();
-    } catch (error) {
-      setError(error);
-    }
-    setLoading(false);
-  };
-
-  const onSearch = () => {
-    setSearch(query);
-  };
-
-  const createDate = (d) => {
-    const date = new Date(d);
-    return date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
-  };
-
   console.log(data);
 
-  return loading ? (
+  return isLoading ? (
     <h1>Loading</h1>
   ) : error ? (
     <h1>{error.message}</h1>
   ) : (
     <>
-      <input value={query} onChange={(e) => setQuery(e.target.value)} />
-      <button type="button" onClick={onSearch}>
-        Search
-      </button>
       <div>
-        {data.map((d) => {
-          const date = createDate(d.created_at);
-          console.log(date);
-          const url = d.url ? d.url : "https://hckrnews.com/";
-          const title = d.title ? d.title : "hckrnews.com";
-          console.log(d.id);
-          // getCommentsCount(d.id);
-          return (
-            <div key={d.created_at}>
-              <a href={d.link}>{d.link_text}</a>
-              <li>{date}</li>
-            </div>
-          );
-        })}
+        <InfiniteScroll
+          dataLength={data.length}
+          next={() => {
+            fetchData(setData, data, setError);
+          }}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {data.map((d) => {
+            return (
+              <div key={d.id}>
+                {console.log(d.id)}
+                <a href={d.link}>{d.link_text}</a>
+                <p>{d.points}</p>
+              </div>
+            );
+          })}
+        </InfiniteScroll>
       </div>
     </>
   );
