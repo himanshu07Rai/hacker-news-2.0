8c21cf0e2b3c58ebbffaead43dfa53a76e45a4b0
Himanshu Rai <raihimanshu22345gmail.com>
2023-01-09 17:04:27 +0530

    date for infinite scrolling fixed

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,9 +4,47 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 let day = 7;
 
+let dateOffset = 24 * 60 * 60 * 1000; //1 days
+var myDate = new Date(2023, 0, 7);
+
+console.log(myDate);
+
+myDate.setTime(myDate.getTime() - dateOffset);
+
+console.log(myDate);
+
+// console.log(myDate.getDate(), myDate.getMonth(), myDate.getFullYear());
+
+const monthMap = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+];
+const dayMap = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"];
+
+const createURLDate = () => {
+  const day =
+    myDate.getDate() < 10 ? dayMap[myDate.getDate()] : myDate.getDate();
+
+  const month = monthMap[myDate.getMonth()];
+
+  const year = myDate.getFullYear();
+
+  return year + month + day;
+};
+
 const fetchData = async (setData, data, setError) => {
   try {
-    const res = await axios.get(`/api/data/2023010${day}.js`, {
+    const res = await axios.get(`/api/data/${createURLDate()}.js`, {
       method: "GET",
       mode: "no-cors",
       headers: {
@@ -20,7 +58,8 @@ const fetchData = async (setData, data, setError) => {
     //   let x = t;
     //   x.sort((a, b) => b.points - a.points);
     setData([...data, ...t]);
-    day = day - 1;
+    // day = day - 1;
+    myDate.setTime(myDate.getTime() - dateOffset);
   } catch (error) {
     setError(error);
   }
@@ -30,15 +69,6 @@ const Homepage = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const date = new Date(2023, 0, 1);
-  console.log(date.getDate(), date.getMonth(), date.getFullYear());
-
-  date.setDate(date.getDate() - 1);
-
-  console.log(date.getDate(), date.getMonth(), date.getFullYear());
-
-  console.log(date);
 
   useEffect(() => {
     setIsLoading(true);
@@ -73,7 +103,7 @@ const Homepage = () => {
           {data.map((d) => {
             return (
               <div key={d.id}>
-                {console.log(d.id)}
+                {/* {console.log(d.id)} */}
                 <a href={d.link}>{d.link_text}</a>
                 <p>{d.points}</p>
               </div>
