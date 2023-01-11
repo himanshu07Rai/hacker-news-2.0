f2911d5f9433a04ff2b003aa9e593bf3af6f9545
Himanshu Rai <raihimanshu22345gmail.com>
2023-01-11 23:50:27 +0530

    Date banner added

--- src/components/DateBanner.jsx
+++ src/components/DateBanner.jsx
@@ -1,7 +1,23 @@
+import { Text } from "@chakra-ui/react";
 import { format } from "date-fns";
 
 const DateBanner = ({ date }) => {
-  return <h1>{format(new Date(date), "dd MMM yyyy")}</h1>;
+  return (
+    <div
+      style={{
+        "box-shadow":
+          " rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px",
+        height: "50px",
+        padding: "10px",
+        backgroundColor: "#f36a6a",
+        borderRadius: "5px",
+      }}
+    >
+      <Text fontSize={"larger"} fontWeight="bold" textColor={"#ffd9d9"}>
+        {format(new Date(date), "dd MMM yyyy")}
+      </Text>
+    </div>
+  );
 };
 
 export default DateBanner;
